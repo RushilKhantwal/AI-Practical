902dfb5c6658b659370648bc5d1a9e3cf8b036e3
Rushil Khantwal <terminator437.rushil@Gmail.com>
2021-05-04 16:03:51 +0530

    Add files via upload

---
+++ AI special/comb.pl
@@ -0,0 +1,26 @@
+combo(A,B,C,D):-
+    and(A,B,T1), or(A,B,T2), nand(T1,T2,C), xor(T1,T2,D).
+
+xor(0,0,0).
+xor(0,1,1).
+xor(1,0,1).
+xor(1,1,0).
+
+not(0,1).
+not(1,0). 
+
+or(0,0,0).
+or(0,1,1).
+or(1,0,1).
+or(1,1,1).
+
+and(0,0,0).
+and(0,1,0).
+and(1,0,0).
+and(1,1,1).
+
+nand(A,B,C):-
+    and(A,B,R),not(R,C).
+
+nor(A,B,C):-
+    or(A,B,R),not(R,C).
